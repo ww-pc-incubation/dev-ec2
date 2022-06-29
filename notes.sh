9c5f5dd217aa071490156f7d6f9441c35ab0d606
Paul Carlton <paul.carlton@weave.works>
2022-06-29 16:47:54 +0100

    instance resource created

--- notes.sh
+++ notes.sh
@@ -8,8 +8,8 @@ terraform -chdir=vpc plan -out /tmp/vpc-plan-$$
 terraform -chdir=vpc apply -auto-approve /tmp/vpc-plan-$$ 
 vpc_id=$(terraform -chdir=vpc output -json vpc | jq -r '.vpc_id')
 az=a
-public_subnet_id=$(terraform -chdir=vpc output -json public_subnets | jq --arg z "$az" '.[$z]')
-private_subnet_id=$(terraform -chdir=vpc output -json private_subnets | jq --arg z "$az" '.[$z]')
+public_subnet_id=$(terraform -chdir=vpc output -json public_subnets | jq -r --arg z "$az" '.[$z]')
+private_subnet_id=$(terraform -chdir=vpc output -json private_subnets | jq -r --arg z "$az" '.[$z]')
 terraform -chdir=instance  init -backend-config="bucket=$bucket_name" -backend-config="key=instance"
 terraform -chdir=instance validate
 terraform -chdir=instance plan -out /tmp/instance-plan-$$ -var "public_subnet_id=$public_subnet_id"
