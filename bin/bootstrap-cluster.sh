5e99b0f5fc5f7d614283fae99f7be92085f79da6
Paul Carlton <paul.carlton@weave.works>
2022-07-13 20:47:12 +0100

    add bootstrap cluster

--- bin/bootstrap-cluster.sh
+++ bin/bootstrap-cluster.sh
@@ -6,31 +6,6 @@ source /etc/ec2-dev/helper-functions.sh
 
 pushd /etc
 
-# export ISSUER_HOSTPATH=s3-$AWS_REGION.amazonaws.com/$CONFIG_BUCKET
-
-# cat <<EOF > discovery.json
-# {
-#     "issuer": "https://$ISSUER_HOSTPATH",
-#     "jwks_uri": "https://$ISSUER_HOSTPATH/keys.json",
-#     "authorization_endpoint": "urn:kubernetes:programmatic_authorization",
-#     "response_types_supported": [
-#         "id_token"
-#     ],
-#     "subject_types_supported": [
-#         "public"
-#     ],
-#     "id_token_signing_alg_values_supported": [
-#         "RS256"
-#     ],
-#     "claims_supported": [
-#         "sub",
-#         "iss"
-#     ]
-# }
-# EOF
-
-# aws s3 cp --acl public-read ./discovery.json s3://$CONFIG_BUCKET/.well-known/openid-configuration
-
 eksctl anywhere create cluster -f /etc/ec2-dev/eksa-bootstrap.yaml
 chmod 644 ${PWD}/eksa-bootstrap/eksa-bootstrap.kubeconfig
 mv ${PWD}/eksa-bootstrap/eksa-bootstrap.kubeconfig /etc/ec2-dev/eksa-bootstrap.kubeconfig
@@ -53,4 +28,5 @@ git branch -M main
 git push --set-upstream https://$GITHUB_TOKEN@github.com/$MGMT_ORG/$MGMT_REPO.git main
 chown -R ec2-user:ec2-user /home/ec2-user/go/src/github.com/$MGMT_ORG/$MGMT_REPO
 flux bootstrap github --owner=$MGMT_ORG --repository=$MGMT_REPO --private=false --personal=false --path clusters/bootstrap
+
 popd
