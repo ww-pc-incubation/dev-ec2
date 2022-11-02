3eccbbac0dab14bd4f349a7be62db243df0d3177
Paul Carlton <paul.carlton@weave.works>
2022-11-02 11:10:27 +0000

    fix clone.sh

--- bin/clone.sh
+++ bin/clone.sh
@@ -12,6 +12,7 @@ function usage()
     echo "where <path> is the repository path, i.e. github.com/weaveworks/weave-gitops" >&2
     echo " will also extract this from https or git repository urls" >&2
     echo "This script will create the appropriate directory sub tree under $HOME/go/src" >&2
+    echo "The base directory can be set to something different by setting BASE_DIR environmental variable" >&2
     echo "then clones the repository if the final element of the path does not exist" >&2
     echo "if the repository directory already exists, it does a pull" >&2
     echo "defaults to using github.com if <path> is no a full url" >&2
@@ -21,6 +22,7 @@ function args() {
   dry_run=""
   fork=""
   server="github.com"
+  base_dir=${BASE_DIR:-$HOME/go/src}
   unset path
 
   arg_list=( "$@" )
@@ -29,7 +31,7 @@ function args() {
   while (( arg_index < arg_count )); do
     case "${arg_list[${arg_index}]}" in
           "--debug") set -x;;
-        "--dry-run") dry_run="echo ";;
+          "--dry-run") dry_run="echo ";;
           "--fork") fork=1;;
                "-h") usage; exit;;
            "--help") usage; exit;;
@@ -62,8 +64,8 @@ function args() {
 
 args "$@"
 
-repo=$(basename "${HOME}/go/src/${path}")
-dir=$(dirname "${HOME}/go/src/${path}")
+repo=$(basename "$base_dir/${path}")
+dir=$(dirname "$base_dir/${path}")
 org=$(basename "${dir}")
 dir=$(dirname "${dir}")
 server=${server:-$(basename "${dir}")}
@@ -71,11 +73,19 @@ if [ -n "${fork}" ] ; then
   org=paulcarlton-ww
 fi
 
-if [ ! -e "${HOME}/go/src/${path}/.git" ] ; then
-    $dry_run mkdir -p "${HOME}/go/src/${path}" >&2
-    $dry_run git clone git@${server}:"${org}"/"${repo}".git "${HOME}/go/src/${path}" >&2
+if [ ! -e "$base_dir/${path}/.git" ] ; then
+    $dry_run mkdir -p "$base_dir/${path}" >&2
+    $dry_run git clone git@${server}:"${org}"/"${repo}".git "$base_dir/${path}" >&2
 fi
 
-echo "${HOME}/go/src/${path}"
-
-$dry_run git -C "${HOME}/go/src/${path}" pull >&2
+echo "$base_dir/${path}"
+pushd "$base_dir/${path}" >/dev/null
+default_branch="$(git symbolic-ref refs/remotes/origin/HEAD | sed 's@^refs/remotes/origin/@@')"
+current_branch="$(git rev-parse --abbrev-ref HEAD)"
+popd >/dev/null
+if [ "$current_branch" == "$default_branch" ]; then
+  $dry_run git -C "$base_dir/${path}" pull 1>&2
+else
+  echo "Warning existing clone of repo, not on default branch, not pulling" >&2
+  exit 1
+fi
